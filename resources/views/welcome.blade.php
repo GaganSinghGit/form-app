e5105b036ed37666f1d7b676349d17c111e2c209
root <root@LAPTOP-FPTEH0C2>
2022-12-08 00:31:45 +1100

    initial commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Inter">
+
+        <!-- @vite('resources/css/app.css') -->
+        <link ref="stylesheet" href="http://localhost:5173/resources/css/app.css" >
+    </head>
+    <body class="antialiased" style="font-family: Inter;">
+        <div id="app"></div>
+        <script type="module" src="http://localhost:5173/resources/js/app.js"></script>
+        <!-- @vite('resources/js/app.js') -->
+    </body>
+</html>
